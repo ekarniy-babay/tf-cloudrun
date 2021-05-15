c48d2b54e075e97c18d73c27b954805ac30e1845
Artem Matviychuk <asm@sysadmins.club>
2021-05-15 09:49:20 +0300

    ok

--- terraform-cloud-run-master/gcp/variables.tf
+++ terraform-cloud-run-master/gcp/variables.tf
@@ -2,11 +2,43 @@ variable "GOOGLE_CREDENTIALS" {
   description = "Needs to be present to suppress TF warning of unused vars"
 }
 
-variable "project_id" {
-  description = "Target project_ID"
-}
-
 variable "app_version" {
   description = "Version to deploy"
   default     = "latest"
 }
+
+# define project ID
+variable "project_id" {
+    type        = string
+    description = "Target project_ID"
+    default     = ""
+}
+
+# define environment type: dev/test/stage/prod
+variable "app_env" {
+    type        = string
+    description = "Environment type"
+    default = "dev"
+}
+
+# define autentication key
+variable "gcp_auth_file" {
+    type        = string
+    description = "Authentication file"    
+    default     = "key.json"
+}
+
+# define GCP Region
+variable "gcp_region_1" {
+    type        = string
+    description = "Default region"
+    default     = "us-central1"
+}
+
+# define GCP zone
+variable "gcp_zone_1" {
+    type        = string    
+    description = "GCP zone"
+    default     = "us-central1-c"
+}
+
